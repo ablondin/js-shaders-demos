1209d1f8cc5069e162b83ff2ee8751d8e0ed820a
Alexandre Blondin Massé <alexandre.blondin.masse@gmail.com>
2015-11-04 11:21:04 -0500

    Ajout d'exemples sur les shaders en three.js.

---
+++ sphere-shader-3/js/main.js
@@ -0,0 +1,83 @@
+// Variables globales
+var scene; 
+var camera; 
+var distance = 15;
+
+// Initialisation de la scène
+initializeScene(); 
+
+// Animation de la scène
+animateScene(); 
+
+function initializeScene(){ 
+    // Initialisation du canvas
+    renderer = new THREE.WebGLRenderer({antialias: true}); 
+    renderer.setClearColor(0x000000, 1); 
+    canvasWidth = 800; 
+    canvasHeight = 600; 
+    renderer.setSize(canvasWidth, canvasHeight); 
+    document.getElementById("canvas").appendChild(renderer.domElement); 
+
+    // Initialisation de la scène et de la caméra
+    scene = new THREE.Scene(); 
+    camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 1, 1000); 
+    camera.position.set(0, distance / 2, distance); 
+    camera.lookAt(scene.position); 
+    scene.add(camera); 
+
+    // De la lumière
+    var ambientLight = new THREE.AmbientLight(0x888888);
+    scene.add(ambientLight);
+    var directionalLight = new THREE.DirectionalLight(0xcccccc, 1);
+    directionalLight.position.set(5, 3, 5);
+    scene.add(directionalLight);
+
+    // Chargement du ciel étoilé
+    var geometry = new THREE.SphereGeometry(20, 32, 32);
+    var material = new THREE.MeshBasicMaterial();
+    material.map = THREE.ImageUtils.loadTexture('assets/starfield.png')
+    material.side = THREE.BackSide
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // Chargement des shaders
+    var vertexShader = document.getElementById("shader-vs").innerHTML; 
+    var fragmentShader = document.getElementById("shader-fs").innerHTML; 
+
+    // Chargement de la sphère
+    geometry = new THREE.SphereGeometry(5, 32, 32);
+
+    // Chargement des attributs
+    var attributes = {
+        displacement: {type: 'f', value: []}
+    };
+    var vertices = geometry.vertices;
+    var values = attributes.displacement.value;
+    for(var v = 0; v < vertices.length; v++) {
+        values.push(Math.random() * 0.3);
+    }
+
+    // Chargement du matériel
+    material = new THREE.ShaderMaterial({
+        attributes: attributes,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    });
+
+    // Ajout du modèle
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+} 
+
+function animateScene() { 
+    var timer = new Date().getTime() * 0.0005;
+    camera.position.x = -distance * Math.cos(timer);
+    camera.position.z = distance * Math.sin(timer);
+    camera.lookAt(scene.position); 
+    requestAnimationFrame(animateScene); 
+    renderScene(); 
+} 
+
+function renderScene(){ 
+    renderer.render(scene, camera); 
+} 
